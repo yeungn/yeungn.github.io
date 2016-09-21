3f8be83f03d983b4135e98eb29199a0aaa83d376
yeungn <nin.yeung@gmail.com>
2016-09-21 08:40:17 -0400

    error getting sqft detail from specific listing. updated menu

--- lib/apt_finder/cli.rb
+++ lib/apt_finder/cli.rb
@@ -18,35 +18,43 @@ class AptFinder::CLI
       puts "Enter the number you would like more info on"
       puts "type 'list' to see the listing"
       puts "type 'exit' the end the search"
+      puts ""
       input = gets.strip.downcase
 
       if input.to_i > 0 && input.to_i <= 10
         doc = Nokogiri::HTML(open(listings[input.to_i - 1][2]))
 
         puts ""
-        puts "Details"
+        puts "Description"
         puts "-----------------------------------------------"
-        puts doc.css('div.name')[0].text.strip
-        puts doc.css('#lease-terms .length')[0].text.strip
-        puts doc.css('div.sqft')[0].text.strip
-        puts doc.css('div.rent')[0].text.strip
-        puts doc.css('div.available-now')[0].text.strip
-        puts "-----------------------------------------------"
-        puts doc.css('div.name')[4].text.strip
-        # binding.pry
-        puts doc.css('#lease-terms .length').text.strip
-        puts doc.css('div.sqft')[4].text.strip
-        puts doc.css('div.rent')[4].text.strip
-        puts doc.css('div.available-now')[4].text.strip
-        puts "-----------------------------------------------"
-        puts doc.css('div.name')[9].text.strip
-        puts doc.css('#lease-terms .length').text.strip
-        puts doc.css('div.sqft')[9].text.strip
-        puts doc.css('div.rent')[9].text.strip
-        puts doc.css('div.available-now')[9].text.strip
+        puts doc.search('section.description').text.strip.gsub(/\n|\r/, '').gsub(/\s+/, ' ')
+        puts ""
+        puts "Enter the number of bedrooms:"
+        bedrooms = gets.strip
+
+        if bedrooms >= "2"
+          puts "not available"
+        elsif bedrooms == "0"
+          puts doc.css('div.name')[0].text.strip
+          puts doc.css('#lease-terms .length')[0].text.strip
+          puts doc.css('div.rent')[0].text.strip
+          puts doc.css('div.available-now')[0].text.strip
+        elsif bedrooms == "1"
+          puts doc.css('div.name')[4].text.strip
+          puts doc.css('#lease-terms .length').text.strip
+          puts doc.css('div.rent')[4].text.strip
+          puts doc.css('div.available-now')[4].text.strip
+        end
+
       elsif input == "list"
         listings
+      elsif input == "exit"
+        exit
       end
     end
+  end
+
+  def exit
+    puts "Goodbye!!!"
   end
 end
